d40a50dcab0f718bccf0a7b886d682d309b18cbc
Polly Kang <pollyk@wharton.upenn.edu>
2018-06-25 08:56:06 -0400

    graph!

--- R script/CurrentE-Rscript.R
+++ R script/CurrentE-Rscript.R
@@ -30,7 +30,7 @@ long.data <- data %>%
 #NOTES FOR ME
 ##Target.Gender: AF is female; AM is male
 ##Target.ID: 04, 12, 14, 27
-##Target.Emotion: AFS=; ANS=; NES=;
+##Target.Emotion: AFS=anxiety; ANS=anger; NES=neutral;
 names(long.data)
 head(long.data)
 
@@ -40,52 +40,84 @@ valence.long.data<-long.data %>%
   dplyr::filter(Dimension.Eval == "valence")
 str(valence.long.data)
 
+#SUBSET Valence-AFS
+valence.AFS.long.data<-long.data %>%
+  dplyr::filter(Dimension.Eval == "valence")%>%
+  dplyr::filter(Target.Emotion == "AFS")
+str(valence.long.data)
+
+#SUBSET Valence-ANS
+valence.ANS.long.data<-long.data %>%
+  dplyr::filter(Dimension.Eval == "valence")%>%
+  dplyr::filter(Target.Emotion == "ANS")
+str(valence.long.data)
+
+#SUBSET Valence-NES
+valence.NES.long.data<-long.data %>%
+  dplyr::filter(Dimension.Eval == "valence")%>%
+  dplyr::filter(Target.Emotion == "NES")
+str(valence.long.data)
 
 #SUBSET ACTIVATION
 activation.long.data<-long.data %>%
   dplyr::filter(Dimension.Eval == "activation")
 
-#VALENCE ANALYSIS
-lm1<-glm(Estimate~Target.Gender+Target.ID+Target.Emotion,data=valence.long.data)
-summary(lm1)
+#SUBSET activation-AFS
+activation.AFS.long.data<-long.data %>%
+  dplyr::filter(Dimension.Eval == "activation")%>%
+  dplyr::filter(Target.Emotion == c("AFS", "NES"))
+str(valence.long.data)
 
-#FACTOR LOAD IF THERE ARE NO DIFFERENCES? Or is that butchering data? Can I collapse - don't have to average to collapse?  
-  dplyr::rowwise() %>%
-  dplyr::mutate(mean.own=mean(c(Own.Sad.Feel, Own.Sad.Display), na.rm = T),
-                mean.T=mean(c(AT_1,AT_2,CT_1,CT_2,ATT2), na.rm = T),
-                mean.Diag=mean(c(Diagnosticity_11,Diagnosticity_12,Diagnosticity_13,Diagnosticity_14), na.rm = T),
-                mean.Agree=mean(c(Agreeableness.BFI_1,Agreeableness.BFI_2,Agreeableness.BFI_3), na.rm = T)) %>%
-  dplyr::mutate(Conditions.PartnerSad= ifelse(is.na(Partner.Neutral) , "Partner.Sad", "Partner.Neutral"))  
+#SUBSET activation-ANS
+activation.ANS.long.data<-long.data %>%
+  dplyr::filter(Dimension.Eval == "activation")%>%
+  dplyr::filter(Target.Emotion == c("ANS" , "NES"))
+str(valence.long.data)
+
+#VALENCE
+
+glm2<-glm(Estimate~Target.Gender+Target.ID+Target.Emotion+Video,data=valence.long.data)
+summary(glm2)
+
+glm2<-glm(Estimate~Target.Emotion+Video,data=valence.long.data)
+summary(glm2)
+
+#ACTIVATION ANALYSIS
+glm1<-glm(Estimate~Target.Gender+Target.ID+Target.Emotion+Video,data=activation.long.data)
+summary(glm1)
 
 
+glm.AFS<-glm(Estimate~Target.Gender+Target.ID+Target.Emotion+Video,data=activation.AFS.long.data)
+summary(glm.AFS)
 
+glm.ANS<-glm(Estimate~Target.Gender+Target.ID+Target.Emotion+Video,data=activation.AFS.long.data)
+summary(glm.ANS)
 
-#maybe do median split and match? David, is there a way to do this? E.g. lm(mean.T~ I(mean.Diag < median(mean.Diag)) +mean.Agree,data=data)
-lm<-lm(mean.T~ (Partner.Sad == "Sad") + mean.own+ mean.Diag+mean.Agree,data=data) #mean own sadness is not significantly different across conditions
-summary(lm)
+#  dplyr::rowwise() %>%
+#  dplyr::mutate(mean.own=mean(c(Own.Sad.Feel, Own.Sad.Display), na.rm = T),
+#                mean.T=mean(c(AT_1,AT_2,CT_1,CT_2,ATT2), na.rm = T),
+#                mean.Diag=mean(c(Diagnosticity_11,Diagnosticity_12,Diagnosticity_13,Diagnosticity_14), na.rm = T),
+#                mean.Agree=mean(c(Agreeableness.BFI_1,Agreeableness.BFI_2,Agreeableness.BFI_3), na.rm = T)) %>%
+#  dplyr::mutate(Conditions.PartnerSad= ifelse(is.na(Partner.Neutral) , "Partner.Sad", "Partner.Neutral"))  
 
-
-median(data$mean.own) #5
-
-lm1<-lm(mean.T~mean.Diag+mean.Agree+Partner.Sad,data=data) # 
-summary(lm1)
+#GRAPHS
 
 #make data long first (mean.own, magnitude)
-data %>% 
-  tidyr::gather(DV, magnitude, c("mean.own", "mean.Diag", "mean.Agree")) %>%
-  dplyr::group_by(Partner.Sad,DV) %>%
-  dplyr::summarize(mean = mean(magnitude),
-                   sd=sd(magnitude,na.rm = T),
+
+valence.long.data %>% 
+  dplyr::group_by(Video,Target.Emotion) %>%
+  dplyr::summarize(mean = mean(Estimate),
+                   sd=sd(Estimate,na.rm = T),
                    se = sd/sqrt(n()),
                    n=n()) %>% 
-  ggplot2::ggplot(., ggplot2::aes(x = Partner.Sad, y = mean,fill=Partner.Sad)) + #fill=what variable you want to vary the fill by
+  ggplot2::ggplot(., ggplot2::aes(x = Target.Emotion, y = mean,fill=Target.Emotion)) + #fill=what variable you want to vary the fill by
   ggplot2::geom_bar(position = 'dodge', stat = 'identity') +
-  ggplot2::geom_errorbar(ggplot2::aes(ymin=mean-se, ymax=mean+se))+
-  ggplot2::facet_grid(~ DV) + #positioning of the boxes
-  ggplot2::geom_text(ggplot2::aes(label=round(mean,2), y = 5, x = Partner.Sad), #how do I get the labels? David
-                     position=ggplot2::position_dodge(width = 0.9),
-                     size= 3) +
-  ggplot2::geom_text(ggplot2::aes(label = paste("n =", n), y = 2, x = Partner.Sad), #added n text to the graph
-                     position = ggplot2::position_dodge(width = 0.9),
-                     size = 3, color = 'white') +
+  ggplot2::geom_errorbar(ggplot2::aes(ymin=mean-se, ymax=mean+se),width=.1,position=ggplot2::position_dodge(.9))+
+  ggplot2::facet_grid(~ Video) + #positioning of the boxes
+  ggplot2::geom_text(ggplot2::aes(label=round(mean,2), y = 5, x = Target.Emotion), #how do I get the labels? David
+                     position=ggplot2::position_dodge(width = 3),
+                     size= 3.5) +
+  ggplot2::geom_text(ggplot2::aes(label = paste("n =", n), y = 2, x = Target.Emotion), #added n text to the graph
+                     position = ggplot2::position_dodge(width = 3),
+                     size = 3, color = 'black') +
   papaja::theme_apa() 
